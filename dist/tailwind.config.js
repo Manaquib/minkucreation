ad4cc89440a9840a89b5403f62ca74a1dabe7db1
Manaquib <quibsites53@gmail.com>
2022-09-18 23:22:26 +0530

    ui changes

---
+++ dist/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'six': ['Six Caps', 'sans-serif'],
+        'pango': ['Work Sans', 'sans-serif', 'font-pango' ]
+      },
+    },
+  },
+  plugins: [],
+}
