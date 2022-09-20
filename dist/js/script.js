91bc5d4b83de10538ff1727021dc5ccb1ec9bc2c
Manaquib <quibsites53@gmail.com>
2022-09-20 09:56:54 +0530

    landing, our team

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 const lights=document.querySelector('.lights')
-const partition=document.querySelector('.partition')
 const camera=document.querySelector('.camera')
 const action=document.querySelector('.action')
+const lightInfo=document.querySelector('.lightInfo')
+const actionInfo=document.querySelector('.actionInfo')
+const cameraInfo=document.querySelector('.cameraInfo')
 const landingAnim=document.querySelector('.anim')
 const landing=document.querySelector('.landing')
 const navbar=document.querySelector('.navbar')
@@ -10,19 +12,26 @@ setTimeout(() => {
     lights.style.display='flex'
     setTimeout(() => {
         camera.style.display='flex'
+        lightInfo.style.opacity="0"
     }, 1500);
     setTimeout(() => {
-        partition.style.display='flex'
         action.style.display='flex'
+        cameraInfo.style.opacity="0"
     }, 3000);
     setTimeout(() => {
         landingAnim.style.position='absolute'
-        landingAnim.style.animation='top 2s ease'
+        lightInfo.style.opacity="1"
+        cameraInfo.style.opacity="1"
+        lightInfo.style.marginTop="-10rem"
+        actionInfo.style.marginTop="10rem"
         landing.style.display='flex'
     }, 5000);
+    setTimeout(() => {
+        landingAnim.style.animation='top 2s ease'
+    }, 6500);
     setTimeout(() => {
         landingAnim.style.display='none'
         navbar.style.display='flex'
         navbar.style.animation='bottom 1s ease'
-    }, 7000);
+    }, 8500);
 }, 1000);
